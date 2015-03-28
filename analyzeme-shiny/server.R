7d9b94f0bb24f43ac54b722333bd499cab59db21
Arun Durvasula <ohblahitsme@yahoo.com>
2015-03-27 22:22:30 -0700

    starting out with health information

--- analyzeme-shiny/server.R
+++ analyzeme-shiny/server.R
@@ -1,5 +1,79 @@
 library(shiny)
+library(jsonlite)
+library(digest)
+library(SNPRelate)
+library(gwascat)
+library(ggbio)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+
+options(shiny.maxRequestSize = -1)
 
 shinyServer(function(input, output) {
+  temp.file <- digest(date())
+  
+  load.file.fn <- reactive({
+    data <- input$inFile
+    path <- as.character(data$datapath)
+    print(typeof(path))
+    if(length(path) == 0) {
+      stop()
+    }
+    print(paste("zcat < ", path, " > ./temp/", temp.file, sep=""))
+    system(paste("unzip -p ", path, " > ./temp/", temp.file, sep=""))
+    d <- read.table(paste("./temp/", temp.file, sep=""), sep="\t", header=FALSE, colClasses=c("character", "character", "numeric", "character"), col.names=c("rsid", "chrom", "position", "genotype"))
+    
+  })
+
+  output$snpsByChr <- renderPlot({
+    withProgress(message="Graphing data", value=0.1, {
+        #     d <- tryCatch({
+        #       load.file.fn()
+        #     }, error = function(err) {
+        d <- read.table("./static/example.txt", sep="\t", header=FALSE, colClasses=c("character", "character", "numeric", "character"), col.names=c("rsid", "chrom", "position", "genotype"))
+        #     })
+        incProgress(0.1)
+        d$chrom = ordered(d$chrom, levels=c(seq(1, 22), "X", "Y", "MT"))
+        incProgress(0.1)
+        setProgress(0.75)
+        ggplot(d, aes(x=chrom, fill=factor(chrom))) + ggtitle("Number of mutations per chromosome") + geom_bar() + theme(legend.position = "none")
+        
+      })
+    })
+  output$riskTable <- renderDataTable({
+    withProgress(message="Finding SNP associations", value=0.1, {
+      d <- tryCatch({
+        load.file.fn()
+      }, error = function(err) {
+        d <- read.table("./static/example.txt", sep="\t", header=FALSE, colClasses=c("character", "character", "numeric", "character"), col.names=c("rsid", "chrom", "position", "genotype"))
+      })
+      incProgress(0.1)
+      d$chrom = ordered(d$chrom, levels=c(seq(1, 22), "X", "Y", "MT"))
+      txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+      levels(d$chrom) <- paste("chr", c(1:22, "X", "Y", "M"), sep="")
+      my.snps <- with(d, GRanges(seqnames=chrom, 
+                                 IRanges(start=position, width=1), 
+                                 rsid=rsid, genotype=genotype))
+      incProgress(0.1)
+      data(gwrngs19)
+      gwrngs.emd <- as.data.frame(elementMetadata(gwrngs19))
+      incProgress(0.1)
+      dm <- merge(d, gwrngs.emd, by.x="rsid", by.y="SNPs")
+      risk.alleles <- gsub("[^\\-]*-([ATCG?])", "\\1", dm$Strongest.SNP.Risk.Allele)
+      i.have.risk <- mapply(function(risk, mine) {
+        risk %in% unlist(strsplit(mine, ""))
+      }, risk.alleles, dm$genotype)
+      incProgress(0.1)
+      dm$i.have.risk <- i.have.risk
+      my.risk <- dm[dm$i.have.risk, ]
+      rel.cols <- c(colnames(d), "Disease.Trait", "Risk.Allele.Frequency",
+                    "p.Value", "i.have.risk", "X95..CI..text.", "Initial.Sample.Size")
+      my.risk.ord <- my.risk[order(my.risk$Risk.Allele.Frequency), rel.cols]
+      risk.table <- my.risk.ord[,c("genotype", "Disease.Trait", "p.Value", "Risk.Allele.Frequency", "Initial.Sample.Size")]
+      setProgress(1)
+      colnames(risk.table) <- c("Genotype", "Disease Trait", "P Value","Risk Allele Frequency", "Population Sampled")
+      risk.table
+    })
+  }, options = list(pageLength = 10))
+  
   
 })
--- analyzeme-shiny/ui.R
+++ analyzeme-shiny/ui.R
@@ -2,10 +2,37 @@ library(shiny)
 
 shinyUI(
   fluidPage(
-    titlePanel("analyzeMe"),
-    mainPanel(
-      p("Have you had your genome analyzed with 23andme? Have you wondered how this data can be analyzed? Well you're in the right place! Upload your raw data here, and we will analyze it and walk through what exactly it is we're looking at. "),
-      p("Have you uploaded your file (or used the example)? Great! The analysis will take a while, but while you're waiting, we can talk about what we're doing!")
+    fluidRow(
+      column(
+        12,
+        offset=1,
+        h1("analyzeMe"),
+        h4("by Arun Durvasula")
+      )
+    ),
+    fluidRow(
+      column(
+          3,
+          offset=1,
+          h3("About"),
+          p("Built with shiny and R")
+        ),
+      column(
+        7,
+        p("Have you had your genome analyzed with 23andme? Have you wondered how this data can be analyzed? Well you're in the right place! Upload your raw data here, and we will analyze it and walk through what exactly it is we're looking at. "),
+        p("Privacy: your data will be deleted in a week and will never be looked at."),
+        fileInput(inputId="inFile", label="Please select a zipped 23andme raw data file.", accept=c("application/x-gzip")),
+        p("or"),
+        checkboxInput(inputId="useExample", label="Use Example", value=T),
+        p("Have you uploaded your file (or used the example)? Great! The analysis will take a while, but while you're waiting, let's talk about what we're doing."),
+        h2("The data"),
+        p("The data you receive from 23 and Me is the result of a genotyping experiment. This is not the same as sequencing, but when you are only trying to find out what mutations exist at single base pairs, it is a much cheaper alternative to sequencing."),
+        p("What's a base pair? Well let's start at the beginning. Our bodies are made up of cells. These cells come in different types (for example: skin cells, brian cells, liver cells) and each one contains several molecules of DNA that make up your genome. Your genome has all the information needed for your body to create proteins (and do other things too). Proteins carry out important functions in your cells like transporting nutrients in and out, breaking down compounds for energy, and replicating your DNA to make more cells."), 
+        p("Sometimes diseases are caused by mutations in genes that create proteins."),
+
+        plotOutput("snpsByChr"),
+        dataTableOutput('riskTable')
+      )
     )
   ) 
 )
